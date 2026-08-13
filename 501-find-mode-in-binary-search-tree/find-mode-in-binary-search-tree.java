393bfb87bb8439a4de938ae46fe66eb4f6135496
Ayush gupta <ag879869@gmail.com>
2026-08-13 22:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (49.83%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -0,0 +1,49 @@
+class Solution {
+    int count = 0;
+    int maxCount = 0;
+    Integer prev = null;
+
+    List<Integer> result = new ArrayList<>();
+
+    public int[] findMode(TreeNode root) {
+        inorder(root);
+
+        int[] ans = new int[result.size()];
+
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+
+        return ans;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        // Count current value
+        if (prev == null || node.val != prev) {
+            count = 1;
+        } else {
+            count++;
+        }
+
+       
+        if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.add(node.val);
+        } 
+        
+        else if (count == maxCount) {
+            result.add(node.val);
+        }
+
+        prev = node.val;
+
+        inorder(node.right);
+    }
+}
